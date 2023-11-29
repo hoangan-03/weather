ac2311fe9308af3e7553497502d5f6677e042286
John Nguyen <john12052003@gmail.com>
2023-11-29 13:07:04 +0700

    .

--- api/weather.js
+++ api/weather.js
@@ -1,33 +1,36 @@
-import axios from "axios";
-import { apiKey } from "../constants";
-const historyEndpoint = params=> `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${params.cityName}&dt=${params.dt}`;
-const forecastEndpoint = params=> `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${params.cityName}&days=${params.days}`;
-const locationsEndpoint = params=> `https://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${params.cityName}`;
-const apiCall = async (endpoint)=>{
-    const options = {
-        method: 'GET',
-        url: endpoint,
-    };
+import axios from 'axios';
+import {apiKey} from '../constants';
+const historyEndpoint = params =>
+  `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${params.cityName}&dt=${params.dt}`;
+const forecastEndpoint = params =>
+  `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${params.cityName}&days=${params.days}`;
+const locationsEndpoint = params =>
+  `https://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${params.cityName}`;
+const apiCall = async endpoint => {
+  const options = {
+    method: 'GET',
+    url: endpoint,
+  };
 
-      try{
-        const response = await axios.request(options);
-        return response.data;
-      }catch(error){
-        console.log('error: ',error);
-        return {};
-    }
-}
+  try {
+    const response = await axios.request(options);
+    return response.data;
+  } catch (error) {
+    console.log('error: ', error);
+    return {};
+  }
+};
 
-export const fetchWeatherForecast = params=>{
-    let forecastUrl = forecastEndpoint(params);
-    return apiCall(forecastUrl);
-}
+export const fetchWeatherForecast = params => {
+  let forecastUrl = forecastEndpoint(params);
+  return apiCall(forecastUrl);
+};
 
-export const fetchLocations = params=>{
-    let locationsUrl = locationsEndpoint(params);
-    return apiCall(locationsUrl);
-}
-export const fetchHistory = params=>{
-    let historyUrl = historyEndpoint(params);
-    return apiCall(historyUrl);
-}
+export const fetchLocations = params => {
+  let locationsUrl = locationsEndpoint(params);
+  return apiCall(locationsUrl);
+};
+export const fetchHistory = params => {
+  let historyUrl = historyEndpoint(params);
+  return apiCall(historyUrl);
+};

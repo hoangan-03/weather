88de0c019b455d2d8d4d209cae0c07c32eaa52e3
John Nguyen <john12052003@gmail.com>
2024-09-25 01:41:25 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import AppNavigation from './src/navigation/temp';
+import AppNavigation from './src/navigation/Navigation';
 
 
 export default function App() {

d16f05ea27123dbf033a61410f6c96b62dedd14c
John Nguyen <john12052003@gmail.com>
2024-09-25 01:41:05 +0700

    rename

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import AppNavigation from './src/navigation/navigation';
+import AppNavigation from './src/navigation/temp';
 
 
 export default function App() {
